fe9a9980848d564f225ef4424bab631e47885b33
giovana <giovanaccolivera@gmail.com>
2024-05-23 20:55:27 -0300

    alterações-estilo-e-javascript

---
+++ orçamentos.js
@@ -0,0 +1,31 @@
+
+const procedimentos= document.querySelector('.procedimentos-orçamento')
+
+const orçamentos = [
+
+    {name:'<b>Solicite um orçamento</b>', src:'./img/orçamento.png' , descriçao: 'Solicite um orçamento rápido e grátis, através de nossos canais de atendimento.'},
+    {name:'<b>Iremos até você</b>', src:'./img/relogio.png' , descriçao: ' Iremos até você, dentre 30 minutos, para diagnóstico do problema. '},
+    {name:'<b>Mão na massa</b>', src:'./img/ferramenta.png' , descriçao: ' Após aprovado, nossos profissionais entram em ação e resolvem seu problema rapidamente. '}, 
+    {name:'<b>Resolvido</b>', src:'./img/verificado.png' , descriçao: ' Tudo volta ao normal, daremos uma garantia de 3 meses sobre o serviço.  '},
+   
+]
+
+let listaorçamento= ''
+
+
+orçamentos.forEach ( passos => {
+    listaorçamento +=
+    `
+
+<li  class="LI2">
+
+<img src=${passos.src}  >
+<figcaption> ${passos.name}</figcaption>
+
+<p> ${passos.descriçao} </p>
+
+</li>
+`
+})
+procedimentos.innerHTML=listaorçamento
+console.log(listaorçamento)
---
+++ serviços.js
@@ -0,0 +1,9 @@
+const serviços = [
+
+    {name:'<b>Desentupimento de pia</b>', src:'./img/desentupindo-pia.jpg' , descriçao: ' Causas: Acúmulo de restos de alimentos, gordura, cabelos e outros detritos. <br> Como ocorre: Os resíduos acumulam-se nos canos,formando obstruções que impedem a passagem da água.'},
+    {name:'<b>Desentupimento de vaso</b>', src:'./img/vaso.jpg' , descriçao: '   Causas: Descarte inadequado de papel higiênico, objetos estranhos ou excesso de material sólido.<br>Como ocorre: Objetos ou materiais sólidos ficam presos na tubulação, bloqueando o fluxo de água e causando o entupimento.'},
+    {name:'<b>Caixa de Gordura</b>', src:'./img/fossa.jpg' , descriçao: ' Causas: Acúmulo de gordura, óleos e resíduos de alimentos na caixa de gordura. <br> Como ocorre: Os materiais acumulados na caixa de gordura solidificam-se com o tempo, formando uma camada espessa que obstrui a passagem da água '}, 
+    {name:'<b>Desentupimento de ralos</b>', src:'./img/ralo.jpg' , descriçao: '  Causas: Acúmulo de cabelos, sujeira, restos de sabão e outros detritos. <br>Como ocorre: Os materiais acumulados na grade do ralo ou dentro da tubulação formam uma obstrução que impede a passagem da água.'},
+    {name:'<b>Desentupimento de esgoto</b>', src:'./img/desentupimento.jpg' , descriçao: ' Causas: Acúmulo de resíduos sólidos, gordura, raízes de árvores e objetos estranhos na rede de esgoto.<br>Como ocorre: Obstrução parcial ou total da rede de esgoto devido ao acúmulo de materiais, resultando em refluxo de água e mau cheiro.'},
+    {name:'<b>Hidrojateamento</b>', src:'./imgcaminhão.jpg' , descriçao: '  Processo: Utilização de alta pressão de água para desobstruir canos e tubulações. <br>Funcionamento: O jato de água de alta pressão remove os resíduos acumulados nas paredes dos canos, restaurando o fluxo normal de água.'},
+]
